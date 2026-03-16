99f69246e3077e4126920e7a14755b8d1b0d577c
kauerds <ramosprodev@gmail.com>
2026-03-16 10:11:17 -0300

    Add Swagger to the project
    Adapt Swagger to JWT by using SwaggerConfiguration

---
+++ src/main/java/krs/auth_user_api/configuration/SwaggerConfiguration.java
@@ -0,0 +1,34 @@
+package krs.auth_user_api.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    /**
+     * The SwaggerConfiguration class is not always necessary, as Swagger by itself provides a functional UI.
+     * In this case, this class purpose is to adapt Swagger into receiving JWT tokens and managing their
+     * authorizations.
+     **/
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        return new OpenAPI()
+                .info(new Info().title("SQL Application API").version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
