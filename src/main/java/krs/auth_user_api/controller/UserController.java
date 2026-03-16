17b88eba8b6c04109381ed3753a007ea39e83c28
kauerds <ramosprodev@gmail.com>
2026-03-16 10:10:24 -0300

    Refactor UserController class with more precise names and role distribution
    Complete class methods, add response codes and remove unused imports

--- src/main/java/krs/auth_user_api/controller/UserController.java
+++ src/main/java/krs/auth_user_api/controller/UserController.java
@@ -1,18 +1,25 @@
 package krs.auth_user_api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
+import krs.auth_user_api.dto.UserDTO;
 import krs.auth_user_api.dto.UserPatchDTO;
 import krs.auth_user_api.entity.UserEntity;
 import krs.auth_user_api.services.UserService;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 public class UserController {
 
     // Manually injecting dependencies.
@@ -22,48 +29,142 @@ public class UserController {
         this.userService = userService;
     }
 
-    // ---READ---
-    @GetMapping("/read/{id}")
-    @PreAuthorize("#id == authentication.principal.id")
-    public ResponseEntity<?> readUser(@Valid @PathVariable String id) {
+    // 1. Create user
+    @Operation(summary = "User creation (ADMIN only)", description = "Allows the ADMIN to create a new user.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully."),
+            @ApiResponse(responseCode = "401", description = "You are not authenticated."),
+            @ApiResponse(responseCode = "403", description = "Request access denied."),
+            @ApiResponse(responseCode = "409", description = "Data provided caused conflict with an existent user."),
+            @ApiResponse(responseCode = "500", description = "Unexpected Server error occurred.")
+    })
+    @PostMapping("/create")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserEntity> createUser(@RequestBody @Valid UserDTO userDTO) {
         try {
-            UserEntity selectedUser = this.userService.readUser(id);
+            UserEntity createdUser = userService.createUser(userDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        } catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    // 2. Read all users
+    @Operation(summary = "Users list reading (ADMIN only)", description = "Allows the ADMIN to read all users.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Users returned successfully."),
+            @ApiResponse(responseCode = "401", description = "You are not authenticated."),
+            @ApiResponse(responseCode = "403", description = "Request access denied."),
+            @ApiResponse(responseCode = "500", description = "Unexpected Server error occurred.")
+    })
+    @GetMapping("/read/all")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<UserEntity>> readAllUsers() {
+        try {
+            List<UserEntity> usersList = userService.readAllUsers();
+            return ResponseEntity.ok(usersList);
+        } catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    // 2.1 Read single user
+    @Operation(summary = "Single user reading (ADMIN only)", description = "Allows the ADMIN to read a single user.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User returned successfully."),
+            @ApiResponse(responseCode = "400", description = "User not found."),
+            @ApiResponse(responseCode = "401", description = "You are not authenticated."),
+            @ApiResponse(responseCode = "403", description = "Request access denied."),
+            @ApiResponse(responseCode = "500", description = "Unexpected Server error occurred.")
+    })
+    @GetMapping("/read/{userId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserEntity> readUser(@PathVariable String userId) {
+        try {
+            UserEntity selectedUser = userService.readUser(userId);
             return ResponseEntity.ok(selectedUser);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         } catch (DataAccessException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
-    // ---UPDATE(PATCH)---
-    @PatchMapping("/update/{id}")
-    @PreAuthorize("#id == authentication.principal.id")
-    public ResponseEntity<String> updateUser(@Valid @PathVariable String id,
-                                             @RequestBody@Valid @NotNull UserPatchDTO userPatchDTO) {
-
+    // 2.2 Read my user
+    @Operation(summary = "Self user reading", description = "Allows any user to read its own data.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User returned successfully."),
+            @ApiResponse(responseCode = "401", description = "You are not authenticated."),
+            @ApiResponse(responseCode = "500", description = "Unexpected Server error occurred.")
+    })
+    @GetMapping("/read/my/user")
+    public ResponseEntity<UserEntity> readMyUser() {
         try {
-            this.userService.updateUser(id, userPatchDTO);
-            return ResponseEntity.ok("User updated successfully.");
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+            UserEntity selectedUser = userService.readMyUser();
+            return ResponseEntity.ok(selectedUser);
         } catch (DataAccessException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
-    // ---DELETE---
-    @DeleteMapping("/delete/{id}")
-    @PreAuthorize("#id == authentication.principal.id")
-    public ResponseEntity<String> deleteUser(@Valid @PathVariable String id) {
+    // 3. Update my user (Patch)
+    @Operation(summary = "Self user update", description = "Allows any user to update its own data.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated user returned successfully."),
+            @ApiResponse(responseCode = "401", description = "You are not authenticated."),
+            @ApiResponse(responseCode = "409", description = "Data provided caused conflict with an existent user."),
+            @ApiResponse(responseCode = "500", description = "Unexpected Server error occurred.")
+    })
+    @PatchMapping("/update")
+    public ResponseEntity<UserEntity> updateMyUser(@RequestBody @Valid UserPatchDTO userPatchDTO) {
         try {
-            this.userService.deleteUser(id);
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("User deleted successfully.");
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+            UserEntity updatedUser = userService.updateMyUser(userPatchDTO);
+            return ResponseEntity.ok(updatedUser);
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
         } catch (DataAccessException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    // 4. Delete a single user (ADMIN only)
+    @Operation(summary = "User deletion (ADMIN only)", description = "Allows the ADMIN to delete any user.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "User deleted successfully."),
+            @ApiResponse(responseCode = "400", description = "User not found."),
+            @ApiResponse(responseCode = "401", description = "You are not authenticated."),
+            @ApiResponse(responseCode = "409", description = "Data provided caused conflict with an existent user."),
+            @ApiResponse(responseCode = "500", description = "Unexpected Server error occurred.")
+    })
+    @DeleteMapping("/delete/{userId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> deleteUser(@PathVariable String userId) {
+        try {
+            userService.deleteUser(userId);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    // 4.1 Delete my user
+    @Operation(summary = "Self user deletion", description = "Allows any user to delete its own data.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "User deleted successfully."),
+            @ApiResponse(responseCode = "401", description = "You are not authenticated."),
+            @ApiResponse(responseCode = "500", description = "Unexpected Server error occurred.")
+    })
+    @DeleteMapping("/delete/my/user")
+    public ResponseEntity<Void> deleteMyUser() {
+        try {
+            userService.deleteMyUser();
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
-
