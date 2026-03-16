8059b1fac9b58625316595aac8b954762442ce95
kauerds <ramosprodev@gmail.com>
2026-03-16 10:13:42 -0300

    Add response codes and refactor returns

--- src/main/java/krs/auth_user_api/controller/AuthController.java
+++ src/main/java/krs/auth_user_api/controller/AuthController.java
@@ -1,17 +1,20 @@
 package krs.auth_user_api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import krs.auth_user_api.dto.LoginDTO;
-import krs.auth_user_api.dto.UserCreationDTO;
+import krs.auth_user_api.dto.RegisterDTO;
 import krs.auth_user_api.entity.UserEntity;
 import krs.auth_user_api.services.TokenService;
 import krs.auth_user_api.services.UserService;
 import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,32 +36,32 @@ public class AuthController {
         this.userService = userService;
     }
 
-    // ---LOGIN---
+    // 1. Login
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody @Valid LoginDTO loginDTO) {
         var authToken = new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword());
         var authentication = this.authenticationManager.authenticate(authToken);
         var user = (UserEntity) authentication.getPrincipal();
         var token = this.tokenService.generateToken(user);
-        return ResponseEntity.ok("User logged in successfully." + "\n" + "Token:\n" + token);
+        return ResponseEntity.ok(token);
     }
 
-    // ---REGISTER---
+    // 2. Register
+    @Operation(summary = "User registration", description = "Allows anyone to create a new user.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully."),
+            @ApiResponse(responseCode = "409", description = "Data provided caused conflict with an existent user."),
+            @ApiResponse(responseCode = "500", description = "Unexpected Server error occurred.")
+    })
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@Valid @RequestBody UserCreationDTO userCreationDTO,
-                                               Authentication authentication) {
-        if (authentication != null && authentication.isAuthenticated()) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("You are already logged in. Please log out first.");
-        }
-
+    public ResponseEntity<UserEntity> registerUser(@RequestBody @Valid RegisterDTO registerDTO) {
         try {
-            UserEntity user = this.userService.createUser(userCreationDTO);
-            return ResponseEntity.status(HttpStatus.CREATED)
-                    .body("User " + user.getUsername() + " (" + user.getId() + ") created successfully.");
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+            UserEntity createdUser = userService.registerUser(registerDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        } catch (DataIntegrityViolationException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
         } catch (DataAccessException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
 
     }
