b579f8cc9bfacf4b7f7267630c5ee318c310f812
kauerds <ramosprodev@gmail.com>
2026-03-18 11:24:31 -0300

    Remove unused existByRole method

--- src/main/java/krs/auth_user_api/repository/UserRepository.java
+++ src/main/java/krs/auth_user_api/repository/UserRepository.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 public interface UserRepository extends MongoRepository<UserEntity, String> {
     boolean existsByUsername(String username);
     Optional<UserEntity> findByUsername(String username);
-    boolean existsByRole(UserRole role);
     boolean existsByCpf(String cpf);
     boolean existsByEmail(String email);
 }
